2654c827873fa64b448c25c6afcba70727a793de
Alifiyah Shahid <shahid.alifiyah@gmail.com>
2024-08-07 20:41:15 -0400

    First Chatify Commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+//Middleware
+app.use(express.json()); //parse incoming requests with JSON Payloads(from request.body)
+app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   //root route
+//   res.send("Server is Ready");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is Running on ${PORT}`);
+});
